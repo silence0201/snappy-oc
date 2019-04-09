35e30e5d7f476ee2dda57f4fe66a9762b87795be
Silence <374619540@qq.com>
2019-04-09 23:47:28 +0800

    Update Pod Test

---
+++ PodTest/Snappy/AppDelegate.m
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.m
+//  Snappy
+//
+//  Created by Silence on 2019/4/9.
+//  Copyright © 2019年 Silence. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    return YES;
+}
+
+@end
---
+++ PodTest/Snappy/ViewController.m
@@ -0,0 +1,48 @@
+//
+//  ViewController.m
+//  SnappyDemo
+//
+//  Created by Silence on 2019/3/16.
+//  Copyright © 2019年 Silence. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "Foundation+Snappy.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self testJSONCompression];
+}
+    
+- (void)testJSONCompression {
+    NSString *JSONPath = [[NSBundle mainBundle] pathForResource:@"example" ofType:@"json"];
+    
+    NSData *original = [NSData dataWithContentsOfFile:JSONPath];
+    
+    NSData *compressedData = [original compressedSnappyData];
+    
+    NSData *decompressedData = [compressedData decompressedSnappyData];
+    
+    NSUInteger orignLen = [original length];
+    NSUInteger compressLen = [compressedData length];
+    
+    float bench = (compressLen * 100.0) / orignLen ;
+    
+    NSLog(@"原数据长度:%ld,压缩后数据长度:%ld,压缩比例:%f",orignLen,compressLen,bench);
+    
+    NSString *decompressedString = [compressedData decompressedSnappyString];
+    
+    NSLog(@"JSON String:%@",decompressedString);
+    
+    NSDictionary *dic = [NSJSONSerialization JSONObjectWithData:decompressedData options:NSJSONReadingMutableContainers error:NULL];
+    NSLog(@"JSON Dictionary:%@",dic);
+}
+
+
+@end
